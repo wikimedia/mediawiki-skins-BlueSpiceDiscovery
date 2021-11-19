9e17b95246b446a2da0dee59eb83e469f6414085
rvogel <vogel@hallowelt.biz>
2021-11-19 13:15:41 +0100

    Catch exception when request url is not available
    ERM20742
    
    [4.1.x]
    
    Change-Id: Ia1e7a2bb51b1ba9fea2dc3922662541b42a9fb33

--- src/HookHandler/SkinTemplateNavigation/CreateContentOptions.php
+++ src/HookHandler/SkinTemplateNavigation/CreateContentOptions.php
@@ -30,6 +30,16 @@ class CreateContentOptions implements SkinTemplateNavigation__UniversalHook {
 	 * @return void
 	 */
 	public function onSkinTemplateNavigation__Universal( $sktemplate, &$links ) :void {
+		/**
+		 * Unfortunately the `VectorTemplateTest::testGetMenuProps` from `Skin:Vector` will break
+		 * in `REL1_35`, as it does not properly clear out all hook handlers.
+		 * See https://github.com/wikimedia/Vector/blob/1b03bafb1267f350ee2b0018da53c31ee0674f92/tests/phpunit/integration/VectorTemplateTest.php#L107-L108
+		 * In later versions this test does not exist anymore and we can remove the bail out again.
+		 * We do not perform any own UnitTests on this class, so bailing out here should be fine.
+		 */
+		if ( defined( 'MW_PHPUNIT_TEST' ) ) {
+			return;
+		}
 		$user = $sktemplate->getSkin()->getUser();
 		$title = $sktemplate->getSkin()->getTitle();
 		if ( $title instanceof Title === false ) {
--- src/HookHandler/SkinTemplateNavigation/ShareOptions.php
+++ src/HookHandler/SkinTemplateNavigation/ShareOptions.php
@@ -3,6 +3,7 @@
 namespace BlueSpice\Discovery\HookHandler\SkinTemplateNavigation;
 
 use MediaWiki\Hook\SkinTemplateNavigation__UniversalHook;
+use MWException;
 use SkinTemplate;
 use Title;
 
@@ -14,9 +15,27 @@ class ShareOptions implements SkinTemplateNavigation__UniversalHook {
 	 * @return void
 	 */
 	public function onSkinTemplateNavigation__Universal( $sktemplate, &$links ) : void {
+		/**
+		 * Unfortunately the `VectorTemplateTest::testGetMenuProps` from `Skin:Vector` will break
+		 * in `REL1_35`, as it does not properly clear out all hook handlers.
+		 * See https://github.com/wikimedia/Vector/blob/1b03bafb1267f350ee2b0018da53c31ee0674f92/tests/phpunit/integration/VectorTemplateTest.php#L107-L108
+		 * In later versions this test does not exist anymore and we can remove the bail out again.
+		 * We do not perform any own UnitTests on this class, so bailing out here should be fine.
+		 */
+		if ( defined( 'MW_PHPUNIT_TEST' ) ) {
+			return;
+		}
 		$title = $sktemplate->getSkin()->getTitle();
 		if ( $title instanceof Title === false ) {
 			return;
+		}
+		try {
+			// `WebRequest::getRequestURL` may fail in some cases (e.g. UnitTests)
+			$requestUrl = $sktemplate->getConfig()->get( 'Server' )
+				. $sktemplate->getRequest()->getRequestURL();
+		}
+		catch ( MWException $ex ) {
+			$requestUrl = $title->getFullURL();
 		}
 
 		$subject = $sktemplate->msg( 'bs-discovery-sidebar-secondary-share-sharebymail-subject' );
@@ -29,7 +48,7 @@ class ShareOptions implements SkinTemplateNavigation__UniversalHook {
 		$body->params(
 			$sktemplate->getConfig()->get( 'Sitename' ),
 			$title->getFullText(),
-			$sktemplate->getConfig()->get( 'Server' ) . $sktemplate->getRequest()->getRequestURL()
+			$requestUrl
 		);
 
 		$body = rawurlencode( $body->text() );
