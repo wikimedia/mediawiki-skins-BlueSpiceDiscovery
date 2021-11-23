a6559b89b82ce55e1be42530e103678f674f0bde
dvogel <daniel.vogel@hallowelt.com>
2021-11-23 08:48:03 +0100

    Move some template links from 'actions' to 'toolbox'
    [4.1.x]
    
    ERM26158
    
    Change-Id: I56ca1018651883d4ad2738ef6c6808a5faa330b6

--- src/HookHandler/SkinTemplateNavigation/CreateContentOptions.php
+++ src/HookHandler/SkinTemplateNavigation/CreateContentOptions.php
@@ -5,7 +5,6 @@ namespace BlueSpice\Discovery\HookHandler\SkinTemplateNavigation;
 use MediaWiki\Hook\SkinTemplateNavigation__UniversalHook;
 use MediaWiki\Permissions\PermissionManager;
 use SkinTemplate;
-use SpecialPage;
 use Title;
 
 class CreateContentOptions implements SkinTemplateNavigation__UniversalHook {
@@ -40,23 +39,15 @@ class CreateContentOptions implements SkinTemplateNavigation__UniversalHook {
 		if ( defined( 'MW_PHPUNIT_TEST' ) ) {
 			return;
 		}
+
 		$user = $sktemplate->getSkin()->getUser();
 		$title = $sktemplate->getSkin()->getTitle();
 		if ( $title instanceof Title === false ) {
 			return;
 		}
 
-		if ( $this->permissionManager->userHasRight( $user, 'createpage' ) ) {
-			$links['actions']['new-file'] = [
-				'text' => $sktemplate->msg( 'bs-discovery-create-button-new-file-text' ),
-				'title' => $sktemplate->msg( 'bs-discovery-create-button-new-file-title' ),
-				'href' => SpecialPage::getTitleFor( 'Upload' )->getLocalURL(),
-				'class' => 'new-file'
-			];
-		}
-
 		if ( $this->permissionManager->userCan( 'edit', $user, $title ) ) {
-			$links['actions']['new-section'] = [
+			$links['namespaces']['new-section'] = [
 				'text' => $sktemplate->msg( 'bs-discovery-create-button-new-section-text' ),
 				'title' => $sktemplate->msg( 'bs-discovery-create-button-new-section-title' ),
 				'href' => $sktemplate->getTitle()->getLocalURL( [
