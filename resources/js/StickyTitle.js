094e24793495f69c93130d3e3315c5e2ad457ac2
miriam schlindwein <schlindwein@hallowelt.com>
2021-12-01 15:26:31 +0100

    Fix sticky long titles for VE
    ERM26760
    
    [4.x]
    
    Change-Id: If5d9bc6bb8866668d519d24219bd4eb992388c9c

--- resources/js/StickyTitle.js
+++ resources/js/StickyTitle.js
@@ -9,9 +9,17 @@
 				var titleWidth = $( '#main' ).innerWidth();
 				$( '#title-line' ).addClass( 'title-fixed' );
 				$( '#title-line' ).width( titleWidth );
+				if ( $( '.ve-init-target-visual > .ve-ui-toolbar-floating > .oo-ui-toolbar-bar' ) ||
+					$( '.ve-init-target-source > .ve-ui-toolbar-floating > .oo-ui-toolbar-bar ') ) {
+						var topHeight = $( '#title-line' ).height() + $( '#nb-pri' ).height();
+						$( '.ve-init-target-visual > .ve-ui-toolbar-floating > .oo-ui-toolbar-bar' ).css( 'top', topHeight );
+						$( '.ve-init-target-source > .ve-ui-toolbar-floating > .oo-ui-toolbar-bar ').css( 'top', topHeight );
+					}
 			} else {
 				$( '#title-line' ).removeClass( 'title-fixed' );
 				$( '#title-line' ).width( '100%' );
+				$( '.ve-init-target-visual .oo-ui-toolbar-bar' ).css( 'top', '' );
+				$( '.ve-init-target-source .oo-ui-toolbar-bar ').css( 'top', '' );
 			}
 		});
 	}
