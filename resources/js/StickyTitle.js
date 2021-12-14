c8b15a876db882aa9fdde90ab9eec92eea2a011f
miriam schlindwein <schlindwein@hallowelt.com>
2021-12-14 15:29:00 +0100

    Fix jumpmarks with title
    ERM26988
    
    [4.x]
    
    Change-Id: Ib627211f4007da9e0571b5f345dfcd9c13760b91

--- resources/js/StickyTitle.js
+++ resources/js/StickyTitle.js
@@ -1,4 +1,4 @@
-( function( mw, $, d, undefined ){
+( function( mw, $, d ){
 
 	if( !$( 'body' ).hasClass('ns-special') ) {
 		$( window ).scroll( function() {
@@ -40,22 +40,6 @@
 					}
 				}
 			}
-		});
-
-		$( d ).on( 'click', '.toc a', function ( e ) {
-			var topHeight = $( '#title-line' ).height() + $( '#nb-pri' ).height();
-			var target = $( this ).attr( 'href' );
-			var id = $( target ).attr( 'id' );
-
-			if ( !id ) {
-				var parent = $( document.getElementById( target.substr(1) ) );
-			} else {
-				var parent = $( '#' + id );
-			}
-
-			$( 'body,html' ).animate( {
-					scrollTop: parent.position().top - 1.75 * topHeight
-			});
 		});
 	}
 
