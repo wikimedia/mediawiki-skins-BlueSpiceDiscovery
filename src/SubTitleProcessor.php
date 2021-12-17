d1c4b886ec90437f89abcdadfc9bf63d2d0b3c3f
miriam schlindwein <schlindwein@hallowelt.com>
2021-12-17 15:21:18 +0100

    Add backlinks
    ERM27058
    
    [4.x]
    
    Change-Id: I401d1945ec1018403f35a42f35dfb1e279149768

---
+++ src/SubTitleProcessor.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace BlueSpice\Discovery;
+
+class SubTitleProcessor {
+
+	/**
+	 *
+	 * @var string
+	 */
+	private $subtitle = "";
+
+	/**
+	 *
+	 * @var string
+	 */
+	private $subpages = "";
+
+	/**
+	 *
+	 * @var string
+	 */
+	private $backlink = "";
+
+	/**
+	 *
+	 * @var string
+	 */
+	private $redirect = "";
+
+	public function __construct() {
+	}
+
+	/**
+	 * @param string $subtitle
+	 */
+	public function parse( $subtitle ) {
+		$this->subtitle = $subtitle;
+		$this->checkRedirect();
+		$this->checkSubpages();
+		$this->checkBacklink();
+	}
+
+	/**
+	 * @param string $key
+	 * @return string
+	 */
+	public function get( $key = '' ) {
+		if ( $key === 'redirect' ) {
+			return $this->redirect;
+		}
+		if ( $key === 'backlink' ) {
+			return $this->backlink;
+		}
+		if ( $key === 'subpages' ) {
+			return $this->subpages;
+		}
+		if ( $this->subtitle === '/v' ||
+			$this->subtitle === '<br>' ) {
+			$this->subtitle = '';
+		}
+		return $this->subtitle;
+	}
+
+	/**
+	 *
+	 * @return void
+	 */
+	private function checkRedirect() {
+		$hasRedirect = preg_match(
+			'#(<span class="mw-redirectedfrom">)(.*?)(<\/span>)#',
+			$this->subtitle,
+			$matches
+		);
+		if ( $hasRedirect ) {
+			$this->redirect = $matches[0];
+			$this->subtitle = preg_replace(
+			'#(<span class="mw-redirectedfrom">)(.*?)(<\/span>)#',
+			'',
+			$this->subtitle );
+		}
+		$hasRedirectSub = preg_match(
+			'#(<span id="redirectsub">)(.*?)(<\/span>)#',
+			$this->subtitle,
+			$matchesSub
+		);
+		if ( $hasRedirectSub ) {
+			$redirectSub = $matchesSub[0];
+			$this->subtitle = preg_replace(
+				'#(<span id="redirectsub">)(.*?)(<\/span>)#',
+				'',
+				$this->subtitle );
+		}
+	}
+
+	/**
+	 *
+	 * @return void
+	 */
+	private function checkSubpages() {
+		$hasSubpages = preg_match(
+			'#(<span class="subpages">)(.*?)(<\/span>)#',
+			$this->subtitle,
+			$matches
+		);
+		if ( $hasSubpages ) {
+			$this->subpages = $matches[0];
+			$this->subtitle = preg_replace(
+			'#(<span class="subpages">)(.*?)(<\/span>)#',
+			'',
+			$this->subtitle );
+		}
+	}
+
+	/**
+	 *
+	 * @return void
+	 */
+	private function checkBacklink() {
+		$hasBacklink = preg_match(
+			'#(← <a .*?>)(.*?)(<\/a>)#',
+			$this->subtitle,
+			$matches
+		);
+		if ( $hasBacklink ) {
+			$this->backlink = $matches[0];
+			$this->subtitle = preg_replace(
+			'#(← <a .*?>)(.*?)(<\/a>)#',
+			'',
+			$this->subtitle );
+		}
+	}
+}
---
+++ tests/phpunit/SubTitleProcessorTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace BlueSpice\Discovery\Tests;
+
+use BlueSpice\Discovery\SubTitleProcessor;
+use MediaWikiIntegrationTestCase;
+
+/**
+ * @group Database
+ */
+class SubTitleProcessorTest extends MediaWikiIntegrationTestCase {
+
+	/**
+	 *
+	 * @return void
+	 */
+	public function addDBDataOnce() {
+		$this->insertPage( 'Dummy' );
+		$this->insertPage( 'Dummy/ABC' );
+		$this->insertPage( 'Dummy/DEF' );
+		$this->insertPage( 'Dummy/DEF/Some_Subpage' );
+		$this->insertPage( 'Dummy/Dumm/eee/f/gh' );
+	}
+
+	/**
+	 *
+	 * @param string $subtitle
+	 * @param string $expectedRedirect
+	 * @param string $expectedBacklink
+	 * @param string $expectedSubText
+	 * @param string $expectedSubpages
+	 * @return void
+	 * @dataProvider provideParseTestData
+	 * @covers \BlueSpice\Discovery\SubTitleProcessor::parse
+	 */
+	public function testParse( $subtitle, $expectedRedirect, $expectedBacklink,
+		$expectedSubText, $expectedSubpages ) {
+		$this->setMwGlobals( [
+			'wgArticlePath' => '/wiki/$1'
+		] );
+
+		$processor = new SubTitleProcessor();
+		$processor->parse( $subtitle );
+
+		$redirect = $processor->get( 'redirect' );
+		$backlink = $processor->get( 'backlink' );
+		$subText = $processor->get();
+		$subPages = $processor->get( 'subpages' );
+
+		$this->assertEquals( $expectedRedirect, $redirect );
+		$this->assertEquals( $expectedBacklink, $backlink );
+		$this->assertEquals( $expectedSubText, $subText );
+		$this->assertEquals( $expectedSubpages, $subPages );
+	}
+
+	public function provideParseTestData() {
+		return [
+			'redirect' => [
+				// phpcs:ignore Generic.Files.LineLength.TooLong
+				'<span class="mw-redirectedfrom">(Weitergeleitet von <a href="/wiki?title=Dummy/DEF;redirect=no" class="mw-redirect" title="Dummy/DEF" data-bs-title="Dummy/DEF">Dummy/DEF</a>)</span><br><span id="redirectsub">Weiterleitung</span>',
+				// phpcs:ignore Generic.Files.LineLength.TooLong
+				'<span class="mw-redirectedfrom">(Weitergeleitet von <a href="/wiki?title=Dummy/DEF;redirect=no" class="mw-redirect" title="Dummy/DEF" data-bs-title="Dummy/DEF">Dummy/DEF</a>)</span>',
+				'',
+				'',
+				''
+			],
+			'backlink-and-redirect' => [
+				// phpcs:ignore Generic.Files.LineLength.TooLong
+				'<span class="mw-redirectedfrom">(Weitergeleitet von <a href="/wiki?title=Dummy/DEF; redirect=no" class="mw-redirect" title="Dummy/DEF" data-bs-title="Dummy/DEF">Dummy/DEF</a>)</span><br><span id="redirectsub">Weiterleitung</span>← <a href="/wiki/index.php/Dummy/ABC" title="Dummy/ABC" data-bs-title="Dummy/ABC">Dummy/ABC</a>',
+				// phpcs:ignore Generic.Files.LineLength.TooLong
+				'<span class="mw-redirectedfrom">(Weitergeleitet von <a href="/wiki?title=Dummy/DEF; redirect=no" class="mw-redirect" title="Dummy/DEF" data-bs-title="Dummy/DEF">Dummy/DEF</a>)</span>',
+				'← <a href="/wiki/index.php/Dummy/ABC" title="Dummy/ABC" data-bs-title="Dummy/ABC">Dummy/ABC</a>',
+				'',
+				''
+			],
+			'subText' => [
+				// phpcs:ignore Generic.Files.LineLength.TooLong
+				'Von WikiSysop (Beobachtungsliste: Änderungen | normal bearbeiten | im Listenformat bearbeiten (Import/Export) | Beobachtungsliste leeren)',
+				'',
+				'',
+				// phpcs:ignore Generic.Files.LineLength.TooLong
+				'Von WikiSysop (Beobachtungsliste: Änderungen | normal bearbeiten | im Listenformat bearbeiten (Import/Export) | Beobachtungsliste leeren)',
+				''
+			],
+			'subpages' => [
+				// phpcs:ignore Generic.Files.LineLength.TooLong
+				'<span class="subpages"><a href="/wiki/index.php/Dummy" title="Dummy" data-bs-title="Dummy">Dummy</a></span>',
+				'',
+				'',
+				'',
+				// phpcs:ignore Generic.Files.LineLength.TooLong
+				'<span class="subpages"><a href="/wiki/index.php/Dummy" title="Dummy" data-bs-title="Dummy">Dummy</a></span>'
+			]
+		];
+	}
+
+}
