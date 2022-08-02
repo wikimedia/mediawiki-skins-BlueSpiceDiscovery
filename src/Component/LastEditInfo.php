4b9ae471b277c31d096da330334ca35553c43da3
rvogel <vogel@hallowelt.biz>
2022-08-02 08:24:47 +0200

    Fix "last editor" link in page header
    ERM29419
    
    [4.1.x]
    
    Change-Id: Ica24a18f97653bc86a3d98973a6b68d2ee0e209f

--- src/Component/LastEditInfo.php
+++ src/Component/LastEditInfo.php
@@ -3,7 +3,6 @@
 namespace BlueSpice\Discovery\Component;
 
 use BlueSpice\Timestamp;
-use HtmlArmor;
 use MediaWiki\Linker\LinkRenderer;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Revision\RevisionRecord;
@@ -118,7 +117,7 @@ class LastEditInfo extends Literal {
 
 		$html = $this->linkRenderer->makeLink(
 			$wikiPage->getTitle(),
-			new HtmlArmor( $timestamp ),
+			$timestamp,
 			[
 				'title' => $timestamp,
 				'aria-label' => $ariaLabel,
@@ -143,9 +142,6 @@ class LastEditInfo extends Literal {
 		$userIdentity = $revision->getUser();
 		$user = User::newFromId( $userIdentity->getId() );
 		$username = $user->getName();
-		if ( $user->getRealName() !== '' ) {
-			$username = $user->getRealName();
-		}
 
 		/* Main_page is created with user id 0 */
 		if ( $userIdentity->getId() === 0 ) {
@@ -164,7 +160,7 @@ class LastEditInfo extends Literal {
 
 		$html = $this->linkRenderer->makeLink(
 			$user->getUserPage(),
-			new HtmlArmor( $username ),
+			$username,
 			[
 				'aria-label' => Message::newFromKey( 'bs-discovery-title-last-edit-info-user-aria-label', $username ),
 				'role' => 'link'
