5af84587a9f3004a0116c20b27590f8d3696731c
dvogel <daniel.vogel@hallowelt.com>
2022-11-11 13:05:48 +0100

    Add a subpage tree menu provider based on mwstake library
    [4.x]
    
    Requires a new release of the library
    https://github.com/hallowelt/mwstake-mediawiki-component-commonuserinterface/pull/18
    
    Change-Id: Ice57b39cf7c9f6d1161493490ff77cba5a8eb03f

---
+++ src/Component/SubpageTreePanel.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace BlueSpice\Discovery\Component;
+
+use IContextSource;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\Literal;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\SimpleCard;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\SimpleCardHeader;
+
+class SubpageTreePanel extends SimpleCard {
+
+	/**
+	 *
+	 */
+	public function __construct() {
+		parent::__construct( [] );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getId(): string {
+		return 'subpage-tree-pnl';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getContainerClasses(): array {
+		return [ 'w-100', 'bg-transp' ];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getSubComponents(): array {
+		$id = $this->getId();
+		$headerText = wfMessage( 'bs-discovery-subpage-tree-pnl-header-text' )->text();
+
+		return [
+			new SimpleCard( [
+				'id' => $id,
+				'classes' => [ 'w-100', 'bg-transp' ],
+				'items' => [
+					new SimpleCardHeader( [
+						'id' => $id . '-head',
+						'classes' => [ 'menu-title' ],
+						'items' => [
+							new Literal(
+								$id . '-head',
+								$headerText
+							)
+						]
+					] ),
+					new SubpageTree( [
+						'aria' => [
+							'labelledby' => $id . '-head'
+						]
+					] )
+				]
+			] )
+		];
+	}
+
+	/**
+	 *
+	 * @param IContextSource $context
+	 * @return bool
+	 */
+	public function shouldRender( IContextSource $context ): bool {
+		$title = $context->getTitle();
+
+		if ( !$title->isContentPage() ) {
+			return false;
+		}
+
+		return true;
+	}
+}
