14867859477c15b67f233f686922e5ba81fdace2
dvogel <daniel.vogel@hallowelt.com>
2023-04-06 19:09:04 +0200

    Add page version pager
    [4.2.x]
    
    ERM32228
    
    Change-Id: I6d1daa5c6aa5fe039f5784a7e600df27fc4ce404

---
+++ src/AlertProvider/PageVersionPagerAlert.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace BlueSpice\Discovery\AlertProvider;
+
+use BlueSpice\AlertProviderBase;
+use BlueSpice\Discovery\PageVersionPager;
+use BlueSpice\IAlertProvider;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\RevisionRecord;
+use Message;
+
+class PageVersionPagerAlert extends AlertProviderBase {
+
+	/**
+	 *
+	 * @var RevisionRecord|null
+	 */
+	protected $oldRevisionRecord = null;
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getHTML() {
+		$title = $this->skin->getTitle();
+		if ( !$title ) {
+			return '';
+		}
+		$this->initOldId();
+
+		$articleId = $title->getArticleID();
+		$isBadTitle = $articleId === 0;
+		if ( $this->isOldVersion() && !$isBadTitle ) {
+			$message = Message::newFromKey( 'bs-discovery-alert-old-page-version' );
+			$message->params( $articleId );
+
+			$versionPagerHTML = $this->buildPageVersionPager();
+
+			return $message->parse() . $versionPagerHTML;
+		}
+
+		return '';
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getType() {
+		return IAlertProvider::TYPE_INFO;
+	}
+
+	/**
+	 *
+	 * @return bool
+	 */
+	private function isOldVersion() {
+		if ( $this->oldRevisionRecord === null ) {
+			return false;
+		}
+		$currentRevId = $this->skin->getTitle()->getLatestRevID();
+		if ( $this->oldRevisionRecord->getId() === $currentRevId ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function buildPageVersionPager() {
+		$services = MediaWikiServices::getInstance();
+		$revisionLookup = $services->getRevisionLookup();
+		$userLang = $this->skin->getLanguage();
+		$title = $this->skin->getTitle();
+
+		$pageVersionPager = new PageVersionPager( $title, $revisionLookup, $userLang );
+
+		return $pageVersionPager->getHtml( $this->oldRevisionRecord );
+	}
+
+	/**
+	 * Resolves the currently displayed "oldRevisionRecord" from the "old revision view" URL parameters
+	 */
+	private function initOldId() {
+		$oldId = $this->skin->getRequest()->getInt( 'oldid', -1 );
+		$revisionLookup = MediaWikiServices::getInstance()->getRevisionLookup();
+		$this->oldRevisionRecord = $revisionLookup->getRevisionById( $oldId );
+		if ( $this->oldRevisionRecord !== null ) {
+			$direction = $this->skin->getRequest()->getVal( 'direction', '' );
+			if ( $direction === 'next' ) {
+				$this->oldRevisionRecord = $revisionLookup->getNextRevision( $this->oldRevisionRecord );
+			}
+			if ( $direction === 'prev' ) {
+				$this->oldRevisionRecord = $revisionLookup->getPreviousRevision( $this->oldRevisionRecord );
+			}
+		}
+	}
+}
---
+++ src/PageVersionPager.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace BlueSpice\Discovery;
+
+use Html;
+use Language;
+use MediaWiki\Revision\RevisionLookup;
+use MediaWiki\Revision\RevisionRecord;
+use Message;
+use Title;
+use User;
+
+class PageVersionPager {
+
+	/** @var Title */
+	private $title;
+
+	/** @var RevisionLookup */
+	protected $revisionLookup;
+
+	/** @var Language */
+	protected $userLang = null;
+
+	/**
+	 * @param Title $title
+	 * @param RevisionLookup $revisionLookup
+	 * @param Language $userLang
+	 */
+	public function __construct( Title $title, RevisionLookup $revisionLookup, Language $userLang ) {
+		$this->title = $title;
+		$this->revisionLookup = $revisionLookup;
+		$this->userLang = $userLang;
+	}
+
+	/**
+	 * @param RevisionRecord|null $currentRevision
+	 * @return RevisionRecord|null
+	 */
+	private function getNextRevisionRecord( ?RevisionRecord $currentRevision ): ?RevisionRecord {
+		return $this->revisionLookup->getNextRevision( $currentRevision );
+	}
+
+	 /**
+	  * @param RevisionRecord|null $currentRevision
+	  * @return RevisionRecord|null
+	  */
+	private function getPreviousRevisionRecord( ?RevisionRecord $currentRevision ): ?RevisionRecord {
+		return $this->revisionLookup->getPreviousRevision( $currentRevision );
+	}
+
+	/**
+	 * @param RevisionRecord|null $currentRevision
+	 * @return array
+	 */
+	private function getNextRevisionButtonParams( ?RevisionRecord $currentRevision ): array {
+		$params = [
+			'id' => 'bs-page-version-pager-next',
+		];
+
+		$nextRevision = $this->getNextRevisionRecord( $currentRevision );
+		if ( !$nextRevision ) {
+			$params['disabled'] = 'true';
+		} else {
+			$nextRevisionId = $nextRevision->getId();
+			$params['href'] = $this->title->getLinkURL( [ 'oldid' => $nextRevisionId ] );
+		}
+
+		return $params;
+	}
+
+	 /**
+	  * @param RevisionRecord|null $currentRevision
+	  * @return array
+	  */
+	private function getPreviousRevisionButtonParams( ?RevisionRecord $currentRevision ): array {
+		$params = [
+			'id' => 'bs-page-version-pager-previous',
+		];
+
+		$previousRevision = $this->getPreviousRevisionRecord( $currentRevision );
+		if ( !$previousRevision ) {
+			$params['disabled'] = 'true';
+		} else {
+			$previousRevisionId = $previousRevision->getId();
+			$params['href'] = $this->title->getLinkURL( [ 'oldid' => $previousRevisionId ] );
+		}
+
+		return $params;
+	}
+
+	/**
+	 * @param RevisionRecord|null $currentRevision
+	 * @return string
+	 */
+	private function getRevisionInfo( ?RevisionRecord $currentRevision ): string {
+		if ( !$currentRevision ) {
+			return '';
+		}
+
+		/** @var User */
+		$user = $currentRevision->getUser();
+		$userName = $user
+			? $user->getName()
+			: Message::newFromKey( 'bs-discovery-page-version-pager-deleted-user' )->plain();
+
+		$timestamp = $currentRevision->getTimestamp();
+		$date = $this->userLang->date( $timestamp, true );
+		$time = $this->userLang->time( $timestamp, true );
+
+		$message = Message::newFromKey( 'bs-discovery-page-version-pager-info-text' );
+		$message->params( $date, $time, $userName );
+		return $message->parse();
+	}
+
+	/**
+	 * @param RevisionRecord|null $currentRevision
+	 * @return string
+	 */
+	public function getHtml( $currentRevision ): string {
+		$html = Html::openElement( 'div', [ 'class' => 'bs-revision-pager' ] );
+
+		/** info */
+		$html .= Html::openElement( 'div', [ 'class' => 'bs-page-version-pager-info' ] );
+		$html .= $this->getRevisionInfo( $currentRevision );
+		$html .= Html::closeElement( 'div' );
+
+		/** toolbar */
+		$html .= Html::openElement( 'div', [ 'class' => 'bs-page-version-pager-toolbar' ] );
+		$html .= Html::element(
+			'a',
+			$this->getPreviousRevisionButtonParams( $currentRevision ),
+			Message::newFromKey( 'bs-discovery-page-version-pager-previous' )->plain()
+		);
+		$html .= Html::element(
+			'a',
+			$this->getNextRevisionButtonParams( $currentRevision ),
+			Message::newFromKey( 'bs-discovery-page-version-pager-next' )->plain()
+		);
+		$html .= Html::closeElement( 'div' );
+
+		$html .= Html::closeElement( 'div' );
+
+		return $html;
+	}
+}
