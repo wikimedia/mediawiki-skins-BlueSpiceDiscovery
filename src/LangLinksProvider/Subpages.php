5c0681984d72fb0a7e35c7f95eaf8ae763be9156
d.savuljesku <d.savuljesku@gmail.com>
2023-04-27 10:04:44 +0200

    Extend language selector for "Subpages" mode
    - Allow showing language selector when on a subpage for a language
    - Handle sub-sub pages, not all of which are language pages
    - Set pageLanguage to correct code, if applicable
    
    [4.x]
    
    ERM30433
    
    Change-Id: I542a791f8d60fc360570a0230a5335b4d1709288

--- src/LangLinksProvider/Subpages.php
+++ src/LangLinksProvider/Subpages.php
@@ -3,19 +3,25 @@
 namespace BlueSpice\Discovery\LangLinksProvider;
 
 use BlueSpice\Discovery\ILangLinksProvider;
-use MediaWiki\Languages\languageNameUtils;
+use MediaWiki\Languages\LanguageNameUtils;
+use PageProps;
 use Title;
 
 class Subpages implements ILangLinksProvider {
 
-	/** @var languageNameUtils */
-	private $languageNameUtils = null;
+	/** @var LanguageNameUtils */
+	private $languageNameUtils;
+
+	/** @var PageProps */
+	private $pageProps;
 
 	/**
-	 * @param languageNameUtils $languageNameUtils
+	 * @param LanguageNameUtils $languageNameUtils
+	 * @param PageProps $pageProps
 	 */
-	public function __construct( languageNameUtils $languageNameUtils ) {
+	public function __construct( LanguageNameUtils $languageNameUtils, PageProps $pageProps ) {
 		$this->languageNameUtils = $languageNameUtils;
+		$this->pageProps = $pageProps;
 	}
 
 	/**
@@ -24,27 +30,95 @@ class Subpages implements ILangLinksProvider {
 	 * @return string[]
 	 */
 	public function getLangLinks( array $wikitextLangLinks, Title $title ): array {
-		$titleObject = $title;
+		if ( !$title->exists() ) {
+			return [];
+		}
+		$availableLanguages = $this->getAvailableLanguages( $title );
 		$links = [];
+		foreach ( $availableLanguages as $code => $page ) {
+			$langName = $this->languageNameUtils->getLanguageName( $code, $code );
+			$links[] = [
+				'href' => $page->getLocalURL(),
+				'text' => $langName,
+				'title' => $this->getDisplayText( $page ),
+				'class' => 'interlanguage-link interwiki-' . $code,
+				'link-class' => 'interlanguage-link-target',
+				'lang' => $code,
+				'hreflang' => $code
+			];
+		}
+		return $links;
+	}
+
+	/**
+	 * @param Title $titleObject
+	 *
+	 * @return array
+	 */
+	private function getAvailableLanguages( Title $titleObject ) {
+		$available = [];
+		$currentLangCode = $this->toLangCode( $titleObject->getSubpageText() );
+		// Check if already on the language-subpage, if yes, get the parent to evaluate available langs
+		if ( $currentLangCode ) {
+			$parentTitle = $titleObject->getBaseTitle();
+			$available[$parentTitle->getPageLanguage()->getCode()] = $parentTitle;
+			$titleObject = $parentTitle;
+		}
+
 		if ( $titleObject->hasSubpages() ) {
 			$subpages = $titleObject->getSubpages();
 			foreach ( $subpages as $subpage ) {
-				if ( $this->languageNameUtils->isKnownLanguageTag( $subpage->getSubpageText() ) ) {
-					$langName = $this->languageNameUtils->getlanguageName( $subpage->getSubpageText() );
-					array_push( $links,
-						[
-							'href' => $subpage->getLocalURL(),
-							'text' => $langName,
-							'title' => $titleObject->getPrefixedText() . ' – ' . $langName,
-							'class' => 'interlanguage-link interwiki-' . $subpage->getSubpageText(),
-							'link-class' => 'interlanguage-link-target',
-							'lang' => $subpage->getSubpageText(),
-							'hreflang' => $subpage->getSubpageText()
-						]
-					);
+				if ( !$subpage->exists() || $this->isNestedSubpage( $titleObject, $subpage ) ) {
+					// Subpage does not exist or is a subpage of a subpage
+					continue;
+				}
+				$subpageLangCode = $this->toLangCode( $subpage->getSubpageText() );
+				if ( $subpageLangCode && ( !$currentLangCode || $subpageLangCode !== $currentLangCode ) ) {
+					$available[$subpageLangCode] = $subpage;
 				}
 			}
 		}
-		return $links;
+		return $available;
+	}
+
+	/**
+	 * @param string $text
+	 *
+	 * @return string|null
+	 */
+	private function toLangCode( string $text ): ?string {
+		$code = trim( strtolower( $text ) );
+		if ( $this->languageNameUtils->isKnownLanguageTag( $code ) ) {
+			return $code;
+		}
+		return null;
+	}
+
+	/**
+	 * @param Title $page
+	 *
+	 * @return string
+	 */
+	private function getDisplayText( Title $page ): string {
+		$props = $this->pageProps->getProperties( $page, 'displaytitle' );
+		if ( isset( $props[$page->getArticleID()] ) ) {
+			return $props[$page->getArticleID()];
+		}
+		return $page->getText();
+	}
+
+	/**
+	 * We only want to consider direct subpages of page we are visiting
+	 * We want `A/en`, but not `A/B/en`
+	 * @param Title $titleObject
+	 * @param Title $subpage
+	 *
+	 * @return bool
+	 */
+	private function isNestedSubpage( Title $titleObject, Title $subpage ): bool {
+		$fullText = $titleObject->getText();
+		$subpageText = $subpage->getText();
+		$diff = ltrim( str_replace( $fullText, '', $subpageText ), '/' );
+		return strpos( $diff, '/' ) !== false;
 	}
 }
