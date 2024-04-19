bffedfedcce3226059f5d40e4a321e28313edd00
hslater <hamish.slater@hallowelt.com>
2024-04-19 17:58:08 +1200

    Check for null
    ERM36893
    
    [REL1_39, REL1_39-4.4.x, master]
    
    Change-Id: I2cb767069dcaa81d9c04de19afd0277e7c26809d

--- src/Component/SpecialSpecialPages.php
+++ src/Component/SpecialSpecialPages.php
@@ -2,6 +2,7 @@
 
 namespace BlueSpice\Discovery\Component;
 
+use IContextSource;
 use MediaWiki\MediaWikiServices;
 use Message;
 use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
@@ -10,11 +11,20 @@ use SpecialPage;
 
 class SpecialSpecialPages extends RestrictedTextLink {
 
-	/**
-	 *
-	 */
+	/** @var SpecialPage|null */
+	private $specialPage;
+
 	public function __construct() {
 		parent::__construct( [] );
+		$this->specialPage = MediaWikiServices::getInstance()->getSpecialPageFactory()
+			->getPage( 'Specialpages' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function shouldRender( IContextSource $context ): bool {
+		return (bool)$this->specialPage;
 	}
 
 	/**
@@ -30,9 +40,7 @@ class SpecialSpecialPages extends RestrictedTextLink {
 	 * @return string
 	 */
 	public function getHref(): string {
-		/** @var Title */
-		$specialpage = SpecialPage::getTitleFor( 'Specialpages' );
-		return $specialpage->getLocalURL();
+		return $this->specialPage->getPageTitle()->getLocalURL();
 	}
 
 	/**
@@ -48,9 +56,7 @@ class SpecialSpecialPages extends RestrictedTextLink {
 	 * @return Message
 	 */
 	public function getText(): Message {
-		$specialpage = MediaWikiServices::getInstance()->getSpecialPageFactory()
-		->getPage( 'Specialpages' );
-		return $this->ensureMessageObject( $specialpage->getDescription() );
+		return $this->ensureMessageObject( $this->specialPage->getDescription()) );
 	}
 
 	/**
@@ -58,9 +64,7 @@ class SpecialSpecialPages extends RestrictedTextLink {
 	 * @return Message
 	 */
 	public function getTitle(): Message {
-		$specialpage = MediaWikiServices::getInstance()->getSpecialPageFactory()
-		->getPage( 'Specialpages' );
-		return $this->ensureMessageObject( $specialpage->getDescription() );
+		return $this->ensureMessageObject( $this->specialPage->getDescription() );
 	}
 
 	/**
@@ -68,9 +72,7 @@ class SpecialSpecialPages extends RestrictedTextLink {
 	 * @return Message
 	 */
 	public function getAriaLabel(): Message {
-		$specialpage = MediaWikiServices::getInstance()->getSpecialPageFactory()
-		->getPage( 'Specialpages' );
-		return $this->ensureMessageObject( $specialpage->getDescription() );
+		return $this->ensureMessageObject( $this->specialPage->getDescription() );
 	}
 
 	/**
