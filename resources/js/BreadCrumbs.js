804811daf2552f5bf9774b5ca82ee813b44b6930
Mschlindwein <schlindwein@hallowelt.com>
2024-09-27 13:58:04 +0200

    Fix loading of breadcrumb script
    ERM37538
    
    [4.5.x]
    
    Change-Id: Ia296a59f361f4cac5d8e93bf51d256d57a722616

--- resources/js/BreadCrumbs.js
+++ resources/js/BreadCrumbs.js
@@ -1,41 +1,43 @@
 ( function( d, $, mw ) {
 
-	var dropdownToggles = document.getElementsByClassName( 'breadcrumb-nav-subpages' );
+	$( document ).ready( function( e ) {
+		var dropdownToggles = document.getElementsByClassName( 'breadcrumb-nav-subpages' );
 
-	Array.from( dropdownToggles ).forEach( toggle => {
-		toggle.addEventListener ('show.bs.dropdown', function() {
-			var $breadcrumbItem = $(this).parent( '.breadcrumb-item');
-			if( $breadcrumbItem.data( 'loaded' ) === true ) {
-				return;
-			}
-			var path = $(this).data( 'bs-path' );
-			if( !path ) {
-				return;
-			}
+		Array.from( dropdownToggles ).forEach( toggle => {
+			toggle.addEventListener ('show.bs.dropdown', function() {
+				var $breadcrumbItem = $(this).parent( '.breadcrumb-item');
+				if( $breadcrumbItem.data( 'loaded' ) === true ) {
+					return;
+				}
+				var path = $(this).data( 'bs-path' );
+				if( !path ) {
+					return;
+				}
 
-			var $dropdownMenu = $breadcrumbItem.find('.dropdown-menu > ul').first();
+				var $dropdownMenu = $breadcrumbItem.find('.dropdown-menu > ul').first();
 
-			mw.loader.using( 'mediawiki.api' ).done( function() {
-				var api = new mw.Api();
-				api.abort();
-				api.get( {
-						"format": "json",
-						"action": "bs-wikisubpage-treestore",
-						"node": path,
-						"limit": "-1"
-				})
-				.done( function( response ) {
-					for ( var i = 0; i < response.children.length; i++ ) {
-						$dropdownMenu.append( '<li>' + response.children[i].page_link + '</li>' );
-					};
-					$breadcrumbItem.data( 'loaded', true );
-					var $dropdownItems = $dropdownMenu.find( 'a' );
-					for ( var x = 0; x < $dropdownItems.length; x++ ) {
-						$($dropdownItems[x]).addClass( "dropdown-item" );
-					}
+				mw.loader.using( 'mediawiki.api' ).done( function() {
+					var api = new mw.Api();
+					api.abort();
+					api.get( {
+							"format": "json",
+							"action": "bs-wikisubpage-treestore",
+							"node": path,
+							"limit": "-1"
+					})
+					.done( function( response ) {
+						for ( var i = 0; i < response.children.length; i++ ) {
+							$dropdownMenu.append( '<li>' + response.children[i].page_link + '</li>' );
+						};
+						$breadcrumbItem.data( 'loaded', true );
+						var $dropdownItems = $dropdownMenu.find( 'a' );
+						for ( var x = 0; x < $dropdownItems.length; x++ ) {
+							$($dropdownItems[x]).addClass( "dropdown-item" );
+						}
+					});
 				});
 			});
 		});
-	});
+	} );
 
 })( document, jQuery, mediaWiki );
