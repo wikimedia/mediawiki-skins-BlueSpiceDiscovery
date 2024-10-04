0ba656ba31e01e34a5b9a55db8b0aa3c060f4660
hslater <hamish.slater@hallowelt.com>
2024-10-04 15:42:08 +1300

    Conditionally add the administration card if it is not empty
    ERM38907
    
    [REL1_39, REL1_39-4.5.x, master]
    
    Change-Id: I3d241b464748e7b3f94b0803c53109632c2002c4

--- src/Component/GlobalActionsButton.php
+++ src/Component/GlobalActionsButton.php
@@ -76,88 +76,98 @@ class GlobalActionsButton extends SimpleDropdownIcon implements IRestrictedCompo
 	 * @inheritDoc
 	 */
 	public function getSubComponents(): array {
-		return [
-			new SimpleCard( [
-				'id' => 'ga-mm',
-				'classes' => [
-					'mega-menu', 'd-flex', 'justify-content-center'
-				],
+		$overviewCard = new SimpleCard( [
+			'id' => 'ga-overview',
+			'classes' => [ 'card-mn' ],
+			'items' => [
+				new SimpleCardHeader( [
+					'id' => 'ga-menu-overview-head',
+					'classes' => [ 'menu-title' ],
+					'items' => [
+						new Literal(
+							'ga-menu-title',
+							Message::newFromKey(
+								'bs-discovery-navbar-global-actions-overview-text'
+							)
+						)
+					]
+				] ),
+				new Literal(
+					'ga-menu-list-items',
+					$this->getOverviewSkinSlotHtml()
+				)
+			]
+		] );
+
+		$editingCard = new SimpleCard( [
+			'id' => 'ga-editing',
+			'classes' => [ 'card-mn' ],
+			'items' => [
+				new SimpleCardHeader( [
+					'id' => 'ga-menu-editing-head',
+					'classes' => [ 'menu-title' ],
+					'items' => [
+						new Literal(
+							'ga-menu-title',
+							Message::newFromKey(
+								'bs-discovery-navbar-global-actions-editing-text'
+							)
+						)
+					]
+				] ),
+				new Literal(
+					'ga-menu-list-items',
+					$this->getEditingSkinSlotHtml()
+				)
+			]
+		] );
+
+		$cardBodyItems = [ $overviewCard, $editingCard ];
+
+		// Conditionally add the administration card if the HTML is not empty
+		$administrationHtml = $this->getAdministrationSkinSlotHtml();
+		if ( $administrationHtml !== '' ) {
+			$administrationCard = new SimpleCard( [
+				'id' => 'ga-administration',
+				'classes' => [ 'card-mn' ],
 				'items' => [
-					new SimpleCardBody( [
-						'id' => 'ga-tools-megamn-body',
-						'classes' => [ 'd-flex', 'mega-menu-wrapper' ],
+					new SimpleCardHeader( [
+						'id' => 'ga-menu-administration-head',
+						'classes' => [ 'menu-title' ],
 						'items' => [
-							new SimpleCard( [
-								'id' => 'ga-overview',
-								'classes' => [ 'card-mn' ],
-								'items' => [
-									new SimpleCardHeader( [
-										'id' => 'ga-menu-overview-head',
-										'classes' => [ 'menu-title' ],
-										'items' => [
-											new Literal(
-												'ga-menu-title',
-												Message::newFromKey(
-													'bs-discovery-navbar-global-actions-overview-text'
-												)
-											)
-										]
-									] ),
-									new Literal(
-										'ga-menu-list-items',
-										$this->getOverviewSkinSlotHtml()
-									)
-								]
-							] ),
-							new SimpleCard( [
-								'id' => 'ga-editing',
-								'classes' => [ 'card-mn' ],
-								'items' => [
-									new SimpleCardHeader( [
-										'id' => 'ga-menu-editing-head',
-										'classes' => [ 'menu-title' ],
-										'items' => [
-											new Literal(
-												'ga-menu-title',
-												Message::newFromKey(
-													'bs-discovery-navbar-global-actions-editing-text'
-												)
-											)
-										]
-									] ),
-									new Literal(
-										'ga-menu-list-items',
-										$this->getEditingSkinSlotHtml()
-									)
-								]
-							] ),
-							new SimpleCard( [
-								'id' => 'ga-administration',
-								'classes' => [ 'card-mn' ],
-								'items' => [
-									new SimpleCardHeader( [
-										'id' => 'ga-menu-administration-head',
-										'classes' => [ 'menu-title' ],
-										'items' => [
-											new Literal(
-												'ga-menu-title',
-												Message::newFromKey(
-													'bs-discovery-navbar-global-actions-administration-text'
-												)
-											)
-										]
-									] ),
-									new Literal(
-										'ga-menu-list-items',
-										$this->getAdministrationSkinSlotHtml()
-									),
-								]
-							] )
+							new Literal(
+								'ga-menu-title',
+								Message::newFromKey(
+									'bs-discovery-navbar-global-actions-administration-text'
+								)
+							)
 						]
-					] )
+					] ),
+					new Literal(
+						'ga-menu-list-items',
+						$administrationHtml
+					)
 				]
-			] ),
-			/* literal for transparent megamenu container */
+			] );
+
+			$cardBodyItems[] = $administrationCard;
+		}
+
+		$mainCard = new SimpleCard( [
+			'id' => 'ga-mm',
+			'classes' => [ 'mega-menu', 'd-flex', 'justify-content-center' ],
+			'items' => [
+				new SimpleCardBody( [
+					'id' => 'ga-tools-megamn-body',
+					'classes' => [ 'd-flex', 'mega-menu-wrapper' ],
+					'items' => $cardBodyItems
+				] )
+			]
+		] );
+
+		return [
+			$mainCard,
+			// literal for transparent megamenu container
 			new Literal(
 				'ga-mm-div',
 				'<div id="ga-mm-div" class="mm-bg"></div>'
