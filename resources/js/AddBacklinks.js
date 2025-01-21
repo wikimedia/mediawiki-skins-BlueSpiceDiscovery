a85f766893d21ca46342b9f7f707ff5c25b17343
Mschlindwein <schlindwein@hallowelt.com>
2025-01-21 06:55:03 +0100

    Improve backlinks script
    ERM40112
    
    [5.x]
    
    Change-Id: Ic12d19174bb20844d1a5130563af929c59546fd5
    (cherry picked from commit 7a2606239d626b07f3d860ec4e7160f0e5e795dc)

--- resources/js/AddBacklinks.js
+++ resources/js/AddBacklinks.js
@@ -3,9 +3,17 @@
 		if ( e.currentTarget.title === mw.config.get( 'wgPageName' ) ) {
 			return;
 		}
+		if ( e.currentTarget.href.length === 0 ) {
+			return;
+		}
 		e.preventDefault();
 		var url = e.currentTarget.href;
-		window.location.href = url + '&backTo=' + mw.config.get( 'wgPageName' );
+		var backTo = 'backTo=' + mw.config.get( 'wgPageName' );
+		var connector = '?';
+		if ( url.indexOf( '?' ) > 0 ) {
+			connector = '&';
+		}
+		window.location.href = url + connector + backTo;
 	} );
 
 } )( mediaWiki, jQuery, document );
