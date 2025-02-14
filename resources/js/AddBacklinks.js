6619c6dd9aebb6def782038ab0bb618df69d90bb
Mschlindwein <schlindwein@hallowelt.com>
2025-02-14 14:41:52 +0100

    Fix backTo handler
    ERM40900
    
    [5.x]
    
    Change-Id: Ia1e42352c24bc57ddd758ae598715c57a0146acf

--- resources/js/AddBacklinks.js
+++ resources/js/AddBacklinks.js
@@ -13,14 +13,13 @@
 		if ( href.includes( mw.config.get( 'wgPageName' ) ) ) {
 			return;
 		}
-		e.preventDefault();
 		var url = e.currentTarget.href;
 		var backTo = 'backTo=' + mw.config.get( 'wgPageName' );
 		var connector = '?';
 		if ( url.indexOf( '?' ) > 0 ) {
 			connector = '&';
 		}
-		window.location.href = url + connector + backTo;
+		this.href = url + connector + backTo;
 	} );
 
 } )( mediaWiki, jQuery, document );
