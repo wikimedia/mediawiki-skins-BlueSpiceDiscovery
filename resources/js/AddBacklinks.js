0b8c5eb2f49104b415d3f83a991d2890f36d592f
Mschlindwein <schlindwein@hallowelt.com>
2025-04-30 14:56:31 +0200

    Allow backlink on redlinks
    ERM41562
    
    [5.x]
    
    Change-Id: I71ab375f8f38399e4938eb92d42ce3936c2ab937

--- resources/js/AddBacklinks.js
+++ resources/js/AddBacklinks.js
@@ -1,4 +1,14 @@
 ( function ( mw, $, d ) {
+
+	function buildURL( url ) {
+		const backTo = 'backTo=' + mw.util.rawurlencode( mw.util.rawurlencode( mw.config.get( 'wgPageName' ) ) );
+		let connector = '?';
+		if ( url.indexOf( '?' ) > 0 ) {
+			connector = '&';
+		}
+		return url + connector + backTo;
+	}
+
 	$( d ).on( 'click', '.ns-special #content a', function ( e ) {
 		if ( e.currentTarget.title === mw.config.get( 'wgPageName' ) ) {
 			return;
@@ -13,13 +23,22 @@
 		if ( href.includes( mw.config.get( 'wgPageName' ) ) ) {
 			return;
 		}
-		const url = e.currentTarget.href;
-		const backTo = 'backTo=' + mw.util.rawurlencode( mw.util.rawurlencode( mw.config.get( 'wgPageName' ) ) );
-		let connector = '?';
-		if ( url.indexOf( '?' ) > 0 ) {
-			connector = '&';
+
+		this.href = buildURL( href );
+	} );
+
+	$( d ).on( 'click', '#content a.new', function ( e ) {
+		const href = e.currentTarget.href;
+		if ( href.length === 0 ) {
+			return;
 		}
-		this.href = url + connector + backTo;
+		if ( href.includes( 'returnto' ) ) {
+			return;
+		}
+		if ( href.includes( mw.config.get( 'wgPageName' ) ) ) {
+			return;
+		}
+		this.href = buildURL( href );
 	} );
 
 }( mediaWiki, jQuery, document ) );
