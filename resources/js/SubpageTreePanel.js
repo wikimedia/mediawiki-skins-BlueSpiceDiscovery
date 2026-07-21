744250d5ec3184f0ad6a931c7827d4f9b142e5e0
Mschlindwein <schlindwein@hallowelt.com>
2026-07-21 15:48:04 +0200

    Add subpage tree item action
    Make implicit dependency explicit
    
    ERM48692
    
    [5.x]
    
    Depends-On: If858ee6e418ea2c6c3f747da8001df2a3bb0b127
    Depends-On: I09b54964e72970273cbc351b447f2178c9a8c411
    Change-Id: I948c0391b744ffa9f8f18a67c1728f9715876177

--- resources/js/SubpageTreePanel.js
+++ resources/js/SubpageTreePanel.js
@@ -5,5 +5,12 @@ $( () => {
 		path: 'mws/v1/title-tree-store',
 		skeletonID: 'bs-skin-tree-skeleton'
 	} );
+	const originalUpdatePages = subPageTreePanel.updatePages.bind( subPageTreePanel );
+	subPageTreePanel.updatePages = function () {
+		originalUpdatePages();
+		if ( this.$treeCnt && this.$treeCnt.children().length > 0 ) {
+			this.pageTree.setItemActions( [ new StandardDialogs.ui.AddSubPageItemAction() ] );
+		}
+	};
 	$subpageTreeCnt.append( subPageTreePanel.$element );
 } );
